a89d257e8072bcb8aca7591b0d1dd415801382b6
disamii <samsonmamuye360@gmail.com>
2026-03-22 00:45:03 +0300

    half way there

--- Hard/doubly-linked-list/src/lib.rs
+++ Hard/doubly-linked-list/src/lib.rs
@@ -1,4 +1,4 @@
-use std::thread::current;
+use std::{env::current_exe, thread::current};
 
 // this module adds some functionality based on the required implementations
 // here like: `LinkedList::pop_back` or `Clone for LinkedList<T>`
@@ -33,9 +33,8 @@ impl<T> LinkedList<T> {
             false
         }
     }
+
     pub fn len(&self) -> usize {
-        // error[E0507]: cannot move out of `self.head` which is behind a shared reference
-        // let mut current = self.head;
         let mut current = self.head.as_ref();
         let mut count: usize = 0;
         while let Some(node) = current {
@@ -54,7 +53,7 @@ impl<T> LinkedList<T> {
 
     /// Return a cursor positioned on the back element
     pub fn cursor_back(&mut self) -> Cursor<'_, T> {
-        let mut current  = self.head.as_deref_mut();
+        let mut current = self.head.as_deref_mut();
 
         while current.is_some() {
             current = current.unwrap().next.as_deref_mut();
@@ -64,42 +63,49 @@ impl<T> LinkedList<T> {
 
     /// Return an iterator that moves from front to back
     pub fn iter(&self) -> Iter<'_, T> {
-        Iter{
-            current:self.head.as_deref()
+        Iter {
+            current: self.head.as_deref(),
         }
-        
     }
 }
 
-// the cursor is expected to act as if it is at the position of an element
-// and it also has to work with and be able to insert into an empty list.
 impl<T> Cursor<'_, T> {
-    /// Take a mutable reference to the current element
     pub fn peek_mut(&mut self) -> Option<&mut T> {
-        todo!()
+        self.current.as_mut().map(|node| &mut node.data)
     }
 
-    /// Move one position forward (towards the back) and
-    /// return a reference to the new position
     pub fn next(&mut self) -> Option<&mut T> {
-        todo!()
+        let current = self.current.take()?;
+        self.current = current.next.as_deref_mut();
+        self.current.as_mut().map(|node| &mut node.data)
     }
 
-    /// Move one position backward (towards the front) and
-    /// return a reference to the new position
     pub fn prev(&mut self) -> Option<&mut T> {
-        todo!()
+        let current = self.current.take()?;
+        self.current = current.prev.as_deref_mut();
+        self.current.as_mut().map(|node| &mut node.data)
     }
 
     /// Remove and return the element at the current position and move the cursor
     /// to the neighboring element that's closest to the back. This can be
     /// either the next or previous position.
-    pub fn take(&mut self) -> Option<T> {
-        todo!()
+    pub fn take(&mut self) -> Option<T>
+    where
+        T: Copy,
+    {
+        let current = self.current.take()?;
+        self.current = current.next.as_deref_mut().or(current.prev.as_deref_mut());
+        Some(current.data)
     }
 
     pub fn insert_after(&mut self, _element: T) {
-        todo!()
+        let current = self.current.take();
+        let node = Node {
+            data: _element,
+            next: current.unwrap().next,
+            prev: current,
+        };
+        self.current = Some(node)
     }
 
     pub fn insert_before(&mut self, _element: T) {
