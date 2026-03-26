ba1c00ba844e015bef12cc05fca06bc2df0348f9
disamii <samsonmamuye360@gmail.com>
2026-03-26 15:03:08 +0300

    smart pointer

--- Hard/dominoes/src/lib.rs
+++ Hard/dominoes/src/lib.rs
@@ -11,11 +11,17 @@ pub fn chain(input: &[(u8, u8)]) -> Option<Vec<(u8, u8)>> {
     if num2stone.values().any(|d| d.len() < 2 || d.len() % 2 != 0) {
         return None; // not a Eulerian graph => no chain exists
     }
+    
     let (mut path, mut cycle) = (Vec::new(), Vec::new());
+
+    
     let mut num = match num2stone.keys().next() {
         None => return Some(vec![]),
         Some(&num) => num,
     };
+
+
+
     loop {
         match num2stone.get_mut(&num).unwrap().pop() {
             Some(idx) => {
