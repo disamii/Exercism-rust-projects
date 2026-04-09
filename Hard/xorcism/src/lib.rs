e321a0e73b63981017da2b20bfef8634c1560d03
disamii <samsonmamuye360@gmail.com>
2026-04-09 21:59:54 +0300

    map recovery

--- Hard/xorcism/src/lib.rs
+++ Hard/xorcism/src/lib.rs
@@ -1,38 +1,46 @@
+use std::borrow::Borrow;
+
 /// A munger which XORs a key with some data
 #[derive(Clone)]
 pub struct Xorcism<'a> {
-    // This field is just to suppress compiler complaints;
-    // feel free to delete it at any point.
-    phantom: std::marker::PhantomData<&'a u8>,
+    key: &'a [u8],
+    offset: usize,
 }
 
 impl<'a> Xorcism<'a> {
-    /// Create a new Xorcism munger from a key
-    ///
-    /// Should accept anything which has a cheap conversion to a byte slice.
-    pub fn new<Key>(key: &Key) -> Xorcism<'a> {
-        todo!()
+    pub fn new<Key: AsRef<[u8]> + ?Sized>(key: &'a Key) -> Xorcism<'a> {
+        Self {
+            key: key.as_ref(),
+            offset: 0,
+        }
     }
 
-    /// XOR each byte of the input buffer with a byte from the key.
-    ///
-    /// Note that this is stateful: repeated calls are likely to produce different results,
-    /// even with identical inputs.
+    // pub fn munge_in_place(&mut self, data: &mut [u8]) {
+    //     for byte in data.into_iter() {
+    //         *byte ^= self.key[self.offset];
+    //         self.offset = (self.offset + 1) % self.key.len();
+    //     }
+    // }
     pub fn munge_in_place(&mut self, data: &mut [u8]) {
-        todo!()
+        for byte in data.into_iter() {
+            *byte ^= self.key[self.offset];
+            self.offset = (self.offset + 1) % self.key.len();
+        }
     }
 
-    /// XOR each byte of the data with a byte from the key.
-    ///
-    /// Note that this is stateful: repeated calls are likely to produce different results,
-    /// even with identical inputs.
-    ///
-    /// Should accept anything which has a cheap conversion to a byte iterator.
-    /// Shouldn't matter whether the byte iterator's values are owned or borrowed.
-    pub fn munge<Data>(&mut self, data: Data) -> impl Iterator<Item = u8> {
-        todo!();
-        // this empty iterator silences a compiler complaint that
-        // () doesn't implement ExactSizeIterator
-        std::iter::empty()
+
+    pub fn munge<'b, Data, T>(&'b mut self, data: Data) -> impl Iterator<Item = u8> + 'b
+    where
+        Data: IntoIterator<Item = T> + 'b,
+        T: Borrow<u8>,
+    {
+        data.into_iter().map(|byte| {
+            let ret = byte.borrow() ^ self.key[self.offset];
+            self.offset = (self.offset + 1) % self.key.len();
+            ret
+        })
     }
+
+
 }
+
